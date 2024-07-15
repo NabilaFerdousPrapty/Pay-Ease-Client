f016d4907a56f45d614ff23c7b73b4c12fb4bdfa
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-07-15 20:47:45 +0600

    project initiated

---
+++ src/routes/router.jsx
@@ -0,0 +1,13 @@
+import {
+    createBrowserRouter,
+    
+  } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout/MainLayout";
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout/>,
+    },
+  ]);
+  export default router;
